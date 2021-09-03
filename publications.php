525da3b5a4be9add2a36dbbd8c526c6e7794bc1d
Tyler Shoemaker <tshoemaker@ucdavis.edu>
2021-09-03 12:19:18 -0700

    typo fix

--- publications.php
+++ publications.php
@@ -19,7 +19,7 @@
 		<h3>Publications</h3>
 		<h4>Journal Articles</h4>
 		<p>"<a href="https://scholarworks.iu.edu/journals/index.php/textual/article/view/27153" target="_blank">Error Aligned</a>," <i>Textual Cultures: Texts, Contexts, Interpretations</i> 12:1 (Spring 2019): 155-182</p>
-		<p>"<a href="https://www.researchcatalogue.net/view/380109/380110" target="_blank">Fugue States</a>," <i>Journal of Sonic Studies</i> 16: "Materials of Sound," ed. Caleb kelly (May 2018)</p>
+		<p>"<a href="https://www.researchcatalogue.net/view/380109/380110" target="_blank">Fugue States</a>," <i>Journal of Sonic Studies</i> 16: "Materials of Sound," ed. Caleb Kelly (May 2018)</p>
 		<h4>Book Chapters</h4>
 		<p>"Sonic Materialism and/as Method," <a href="https://www.bloomsbury.com/uk/the-bloomsbury-handbook-of-sonic-methodologies-9781501338755/" target="_blank"><i>The Bloomsbury Handbook of Sonic Methodologies</i></a>, eds. Michael Bull and Marcel Cobussen, London: Bloomsbury, 2020</p>
 		<h4>Essays and Reviews</h4>
