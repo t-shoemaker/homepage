1ecdabf1804cb0052aadf60afe43460de2e5688a
Tyler Shoemaker <tshoemaker@ucdavis.edu>
2021-09-14 10:04:35 -0700

    cv updates; new watereview link

--- research/watereview.php
+++ research/watereview.php
@@ -18,7 +18,7 @@
 	<div id="main-content" class="column">
 		<h3>wateReview</h3>
 		<p><b>Role:</b> Technical Lead (2020–2021)</p>
-		<p><b>Website:</b> <a href="https://datalab-dev.github.io/wateReview_web_app/" target="_blank">datalab-dev.github.io/wateReview_web_app/</a> (temporary web domain)</p>
+		<p><b>Website:</b> <a href="http://watereview.ucdavis.edu" target="_blank">watereview.ucdavis.edu</a></p>
 		<hr>
 		<p>wateReview is a corpus exploration platform that supports a data-driven literature review of water management in the Carribbean and South and Central America (titled <a href="https://hess.copernicus.org/preprints/hess-2021-106/" target="_blank">"Bright and Blind Spots of Water Research in Latin America and the Caribbean"</a>). Working in collaboration with wateReview's domain experts, I built this platform and its many interactive visualizations, which include a topic model viewer, network connectivity graphs, and several chord diagrams. Central to our design considerations was feedback we received from three workshops held in 2020, where we discussed various needs from and for the water research community. These workshops were held in the three major languages of the Caribbean and South and Central America: English, Spanish, and Portuguese. wateReview, in turn, is a multilingual site that offers several multifaceted views of the corpus, ranging from overarching topic distribution plots to individualized "country cards" that break down specific research metrics for each country we studied.</p>
 	</div>
