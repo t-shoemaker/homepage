5a589e9a33ab416e6a88ef4d4e5760e6a3fffa31
Tyler Shoemaker <tshoemaker@ucdavis.edu>
2022-03-23 09:57:24 -0700

    forgot a page

--- research/watereview.php
+++ research/watereview.php
@@ -2,11 +2,11 @@
 <html>
 <head>
 	
-	<title>Tyler Shoemaker • wateReview</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8">
+    <title>Tyler Shoemaker • wateReview</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8">
 	
-	<base href="../">
-	<link href="./theme/style.css" rel="stylesheet">
+    <base href="../">
+    <link href="./theme/style.css" rel="stylesheet">
 	
 </head>
 
@@ -15,13 +15,26 @@
 <?php include("../inc/navbar.php");?>
 
 <div class="row">
-	<div id="main-content" class="column">
-		<h3>wateReview</h3>
-		<p><b>Role:</b> Technical Lead (2020–2021)</p>
-		<p><b>Website:</b> <a href="http://watereview.ucdavis.edu" target="_blank">watereview.ucdavis.edu</a></p>
-		<hr>
-		<p>wateReview is a corpus exploration platform that supports a data-driven literature review of water management in the Caribbean and South and Central America (titled <a href="https://hess.copernicus.org/preprints/hess-2021-106/" target="_blank">"Bright and Blind Spots of Water Research in Latin America and the Caribbean"</a>). Working in collaboration with wateReview's domain experts, I built this platform and its many interactive visualizations, which include a topic model viewer, network connectivity graphs, and several chord diagrams. Central to our design considerations was feedback we received from three workshops held in 2020, where we discussed various needs from and for the water research community. These workshops were held in the three major languages of the Caribbean and South and Central America: English, Spanish, and Portuguese. wateReview, in turn, is a multilingual site that offers several multifaceted views of the corpus, ranging from overarching topic distribution plots to individualized "country cards" that break down specific research metrics for each country we studied.</p>
-	</div>
+    <div id="main-content" class="column">
+        <h3>wateReview</h3>
+        <p><b>Role:</b> Technical Lead (2020–2021)</p>
+        <p><b>Website:</b> <a href="http://watereview.ucdavis.edu"
+        target="_blank">watereview.ucdavis.edu</a></p>
+        <hr>
+        <p>wateReview is a corpus exploration platform that supports a data-driven literature review
+        of water management in the Caribbean and South and Central America (titled <a
+        href="https://hess.copernicus.org/preprints/hess-2021-106/" target="_blank">"Bright and
+        Blind Spots of Water Research in Latin America and the Caribbean"</a>). Working in
+        collaboration with wateReview's domain experts, I built this platform and its many
+        interactive visualizations, which include a topic model viewer, network connectivity graphs,
+        and several chord diagrams. Central to our design considerations was feedback we received
+        from three workshops held in 2020, where we discussed various needs from and for the water
+        research community. These workshops were held in the t hree major languages of the Caribbean
+        and South and Central America: English, Spanish, and Portuguese. wateReview, in turn, is a
+        multilingual site that offers several multifaceted views of the corpus, ranging from
+        overarching topic distribution plots to individualized "country cards" that break down
+        specific research metrics for each country we studied.</p>
+    </div>
 <?php include("../inc/sidebar.php");?>
 
 <figure class="sidebar-fig">
