f192d6bd80694f59c0515acb6273adfd4728915f
Tyler Shoemaker <tshoemaker@ucdavis.edu>
2023-07-17 10:22:47 -0400

    new bio

--- index.php
+++ index.php
@@ -20,12 +20,13 @@
         <h3>About</h3>
         <p>Tyler Shoemaker is a Postdoctoral Scholar affiliated with the <a
         href="https://datalab.ucdavis.edu/" target="_blank">DataLab</a> at the
-        University of California, Davis. He conducts research on media studies
-        and the digital humanities, with a particular emphasis on text
-        technologies, language art, textuality, and the history of information.
-        At the DataLab, Tyler develops and implements methods for text analysis
-        and natural language processing across a variety of research projects,
-        ranging from literary studies to environmental and health sciences.</p>
+        University of California, Davis. A digital humanist, he conducts
+        research on language technology, focusing on how methods in natural
+        language processing (NLP) crosscut the interpretive and theoretic
+        frameworks of literary and media studies. At the DataLab, Tyler
+        develops and implements NLP methods across a variety of research
+        domains, ranging from early modern print to environmental and
+        health sciences.</p>
         <p><a href="docs/shoemaker_cv.pdf">Full CV (.pdf)</a></p>
     </div>
 <?php include("./inc/sidebar.php");?>
