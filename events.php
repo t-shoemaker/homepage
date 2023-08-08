cb6ee6c8b14ad1c95a7dd43e8431c50df0da17fd
Tyler Shoemaker <tshoemaker@ucdavis.edu>
2023-08-08 13:02:50 -0400

    chicago-style entries

--- events.php
+++ events.php
@@ -18,39 +18,42 @@
 <div class="row">
     <div id="main-content" class="column">
         <h3>Lectures and Presentations</h3>
+        <h4>2024</h4>
+        <p>"Good Old-Fashioned Reading." Reading Generative AI:
+        Theory/Data/Critique seminar. Annual conference, Modern Language
+        Association. Philadelpha, January (TBD)
         <h4>2023</h4>
-        <p>"Distributional Semantics without Distributionalism," Digital Theory
-        Lab, New York University, Sept. 29</p>
-        <p>"Indexing Semantic Association," Technolinguistics in Practice,
-        University of Siegen, May 24-26</p>
+        <p>"Distributional Semantics without Distributionalism." Digital Theory
+        Lab. New York University, Sept. 29</p>
+        <p>"Indexing Semantic Association." Technolinguistics in Practice.
+        University of Siegen, May 25</p>
         <h4>2022</h4>
-        <p>Participant, Model Realities seminar, Annual conference, German
-        Studies Association, Houston, Sept. 6-9</p>
-        <p>"Preprocessing the Word," Source Code Criticism seminar, University
-        of Basel, Mar. 25-26</p>
+        <p>Participant, Model Realities seminar. Annual conference, German
+        Studies Association. Houston, Sept. 6-9</p>
+        <p>"Preprocessing the Word." Source Code Criticism seminar, University
+        of Basel. Mar. 26</p>
         <h4>2020</h4>
-        <p>"Plaintext Scrim," Annual conference, Modern Language Association,
+        <p>"Plaintext Scrim." Annual conference, Modern Language Association.
         Seattle, Jan. 9</p>
         <h4>2019</h4>
-        <p>"Script Hacks and Visual Spoofs," Creative Multilingualism
-        conference: "Scriptworlds," University of California, Santa Barbara,
-        May 31</p>
-        <p>"The Edge of Sense," International conference, Society for Textual
-        Scholarship, The New School, New York City, Mar. 21</p>
-        <p>"The Error that Catches the Eye," Annual conference, Modern Language
-        Association, Chicago, Jan. 5</p>
+        <p>"Script Hacks and Visual Spoofs." Creative Multilingualism:
+        Scriptworlds. University of California, Santa Barbara, May 31</p>
+        <p>"The Edge of Sense." Annual conference, Society for Textual
+        Scholarship. The New School, Mar. 21</p>
+        <p>"The Error that Catches the Eye." Annual conference, Modern Language
+        Association. Chicago, Jan. 5</p>
         <h4>2018</h4>
-        <p>"Scanbed Poetics," Annual conference, Electronic Literature
-        Organization, University of Montreal, Aug. 14</p>
-        <p>"Font Futures," Wireframe Studio symposium: "Future Tripping,"
+        <p>"Scanbed Poetics." Annual conference, Electronic Literature
+        Organization. University of Montreal, Aug. 14</p>
+        <p>"Font Futures." Wireframe Studio symposium: Future Tripping.
         University of California, Santa Barabara, Apr. 24</p>
-        <p>"Wordless Labor," Annual conference, Modern Language Association,
+        <p>"Wordless Labor." Annual conference, Modern Language Association.
         New York City, Jan. 6</p>
         <h4>2017</h4>
-        <p>"Linotypes and Linotext Minima," Cal DH work session, University of
+        <p>"Linotypes and Linotext Minima." Cal DH work session. University of
         California, Los Angeles, Nov. 22</p>
-        <p>"Fugue States," Annual conference, American Comparative Literature
-        Association, University of Utrecht, Jul. 6–9</p>
+        <p>"Fugue States." Hearing Things seminar. Annual conference, American
+        Comparative Literature Association. University of Utrecht, Jul. 6–9</p>
     </div>
 <?php include("./inc/sidebar.php");?>
 
