fa52e60d71931a1e6baf3ceda61869f19253b1bd
Tyler Shoemaker <tshoemaker@ucdavis.edu>
2023-09-17 15:51:20 -0700

    updated description for literalism

--- research/literalism.php
+++ research/literalism.php
@@ -18,32 +18,32 @@
 <div class="row">
     <div id="main-content" class="column">
         <h3>Literalism: Reading Machines Reading</h3>
-        <p><i>Literalism</i> traces the contemporary prevalence of large,
-        statistical language models (of the kind, for example, that enable
-        everything from topic modeling to ChatGPT) back to a pivotal moment in
-        the history of computing, when researchers broadly began to think of
-        computers not just as number crunchers, but as generalized symbolic
-        processors that have "access" to meaning. Through a blend of media
-        archaeology, literary close reading, and computational text analysis, I
-        chart how an entire scriptural economy has emerged since the early
-        1950s to support this change in mindset. From blocky, machine-readable
-        typefaces to escape characters, n-grams, and document-term matrices,
-        various quasi-linguistic formalisms have become a key way by which
-        researchers remediate natural language into computationally tractable
-        forms. If today, semantic models are able to provide us with new
-        findings about the nature of language, <i>Literalism</i> shows how such
-        findings are first of all attributable to a skein of strange,
-        intermediary representations of text that never quite present the
-        original features of the data from which they derive.</p>
-        <p>Measuring out the peculiarities of these representations can attune
-        us, I argue, to an interpretive bent that runs through what is typically
-        understood to be the mundane, unthinking work of data wrangling – text
-        cleaning, tokenization, and other such procedures that convert language
-        into forms readable by digital technologies. By challenging
-        distinctions between technical and intellectual decisions within this
-        stratum of data processing, <i>Literalism</i> thus works to critically
-        assess the claims on meaning that statistical language models can and
-        cannot support.</p>
+        <p><i>Literalism</i> traces the advent of today's large, statistical
+        models of language back to a pivotal moment in the history of
+        computing. Models ranging from topic distributions to deep learning
+        networks have their roots in the 1950s, when researchers broadly began
+        to think of computers not just as number crunchers, but as generalized
+        symbolic processors that have access to meaning. A key aspect of this
+        paradigm shift is the way it was supported by an entire scriptural
+        economy, the emergence of which I chart through a combination of media
+        archaeology, close textual analysis, and NLP methods. In doing so,
+        <i>Literalism</i> shows how various textual formalisms have been
+        integral to language modeling from the start. From blocky,
+        machine-readable typefaces to n-grams, document-term matrices, and
+        more, researchers have used these formalisms to remediate natural
+        language into computationally tractable forms. If, today, language
+        modeling has become all but pervasive in its disciplinary reach,
+        concomitant with this expansion is an equally broad proliferation of
+        these strange, intermediary representations of text.</p>
+        <p>Such representations, I argue, comprise a unique kind of textuality.
+        They therefore mark a significant location of semiotic activity that is
+        open to analysis from the vantage of the material text tradition,
+        avant-garde poetics, and media theory. In this sense, NLP requires its
+        own media history. My intent, then, with <i>Literalism</i> is to do
+        just this, demonstrating how computational approaches to language
+        modeling must be extended and enriched by these other disciplinary
+        frameworks, particularly when it comes to assessing the claims on
+        meaning that such models can and cannot support.</p>
     </div>
 <?php include("../inc/sidebar.php");?>
 
