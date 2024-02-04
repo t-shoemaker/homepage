b2d620ca96946c7bd7b482a20e0f9d514714ecb9
Tyler Shoemaker <tshoemaker@ucdavis.edu>
2024-02-03 16:10:51 -0800

    no twitter link

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -4,6 +4,4 @@
         <p>Email: tshoemaker [at] ucdavis [dot] edu</p>
         <p>GitHub: <a href="https://github.com/t-shoemaker"
         target="_blank">t-shoemaker</a><p>
-        <p>Twitter: <a href="https://twitter.com/_tshoemaker"
-        target="_blank">@_tshoemaker</a><p>
     </div>
